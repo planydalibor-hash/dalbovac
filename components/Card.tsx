8aa4d3c4c1ec5648cd65f5fc7fdad005e33f7cf2
Dalibor Planý <planydalibor@gmail.com>
2025-10-15 12:27:04 +0200

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { clsx } from "clsx";
-
+import clsx from "clsx";
 export default function Card({ className, children }: React.PropsWithChildren<{className?: string}>) {
   return <div className={clsx("glass rounded-2xl p-5", className)}>{children}</div>;
 }
